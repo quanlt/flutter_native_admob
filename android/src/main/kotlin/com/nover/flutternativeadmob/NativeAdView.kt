d881af2c314687a716693a968265b1c588bc8f31
quanlt <quanlt.56@gmail.com>
2022-06-01 15:46:19 +0700

    fix mediaview

--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
@@ -7,8 +7,10 @@ import android.graphics.PorterDuff
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
 import com.google.android.gms.ads.nativead.MediaView
 import com.google.android.gms.ads.nativead.NativeAd
 import com.google.android.gms.ads.nativead.NativeAdView
