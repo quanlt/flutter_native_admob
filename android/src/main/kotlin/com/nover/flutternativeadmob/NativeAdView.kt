00d6fb763ee16974e6704f73f0022c7666fcfe88
quanlt <quanlt.56@gmail.com>
2022-06-01 15:49:10 +0700

    fix import

--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
@@ -8,6 +8,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RatingBar
 import android.widget.TextView
