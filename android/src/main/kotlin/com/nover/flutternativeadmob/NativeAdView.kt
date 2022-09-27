e4aca944b6e5d3b539339e7b7117c1422cc9c62c
quanlt <quanlt.56@gmail.com>
2022-09-27 10:27:33 +0700

    Test

--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
@@ -8,60 +8,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.google.android.gms.ads.formats.MediaView
-import com.google.android.gms.ads.formats.UnifiedNativeAd
-import com.google.android.gms.ads.formats.UnifiedNativeAdView
+import com.google.android.gms.ads.nativead.MediaView
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdView
 
 enum class NativeAdmobType {
-  full, banner
+    full, banner
 }
 
 class NativeAdView : FrameLayout {
 
-  constructor(context: Context) : super(context) {
-    inflateView()
-  }
-
-  constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-    inflateView()
-  }
-
-  constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
-    context,
-    attrs,
-    defStyle
-  ) {
-    inflateView()
-  }
-
-
-  private fun inflateView() {
-    val view = LayoutInflater.from(context).inflate(R.layout.native_admob_full_view, this, true)
-    view.findViewById<MediaView>(R.id.adMedia).setImageScaleType(ImageView.ScaleType.CENTER_CROP)
-
-    val adView = view.findViewById<UnifiedNativeAdView>(R.id.ad_view)
-    adView.headlineView = view.findViewById<TextView>(R.id.tvHeadline)
-    adView.bodyView = view.findViewById<TextView>(R.id.tvBody)
-    adView.callToActionView = view.findViewById<Button>(R.id.btnAction)
-    adView.iconView = view.findViewById<ImageView>(R.id.imvIcon)
-    adView.priceView = view.findViewById<Button>(R.id.tvPrice)
-    adView.storeView = view.findViewById<Button>(R.id.tvStore)
-    adView.advertiserView = view.findViewById<Button>(R.id.tvAdvertiser)
-    adView.mediaView = view.findViewById(R.id.adMedia)
-  }
-
-  fun setNativeAd(nativeAd: UnifiedNativeAd?) {
-    val adView = findViewById<UnifiedNativeAdView>(R.id.ad_view)
-    (adView.headlineView as TextView).text = nativeAd?.headline
-    (adView.bodyView as TextView).text = nativeAd?.body
-    (adView.callToActionView as Button).text = nativeAd?.callToAction
-    (adView.iconView as ImageView).setImageDrawable(nativeAd?.icon?.drawable)
-    (adView.priceView as TextView).text = nativeAd?.price
-    (adView.storeView as TextView).text = nativeAd?.store
-    (adView.advertiserView as TextView).text = nativeAd?.advertiser
-    nativeAd?.mediaContent?.let {
-      adView.mediaView?.setMediaContent(it)
+    constructor(context: Context) : super(context) {
+        inflateView()
     }
-    adView.setNativeAd(nativeAd)
-  }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        inflateView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        inflateView()
+    }
+
+
+    private fun inflateView() {
+        val view = LayoutInflater.from(context).inflate(R.layout.native_admob_full_view, this, true)
+        view.findViewById<MediaView>(R.id.adMedia)
+            .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+
+        val adView = view.findViewById<NativeAdView>(R.id.ad_view)
+        adView.headlineView = view.findViewById<TextView>(R.id.tvHeadline)
+        adView.bodyView = view.findViewById<TextView>(R.id.tvBody)
+        adView.callToActionView = view.findViewById<Button>(R.id.btnAction)
+        adView.iconView = view.findViewById<ImageView>(R.id.imvIcon)
+        adView.priceView = view.findViewById<Button>(R.id.tvPrice)
+        adView.storeView = view.findViewById<Button>(R.id.tvStore)
+        adView.advertiserView = view.findViewById<Button>(R.id.tvAdvertiser)
+        adView.mediaView = view.findViewById(R.id.adMedia)
+    }
+
+    fun setNativeAd(nativeAd: NativeAd?) {
+        nativeAd ?: return
+        val adView = findViewById<NativeAdView>(R.id.ad_view)
+        (adView.headlineView as TextView).text = nativeAd?.headline
+        (adView.bodyView as TextView).text = nativeAd?.body
+        (adView.callToActionView as Button).text = nativeAd?.callToAction
+        (adView.iconView as ImageView).setImageDrawable(nativeAd?.icon?.drawable)
+        (adView.priceView as TextView).text = nativeAd?.price
+        (adView.storeView as TextView).text = nativeAd?.store
+        (adView.advertiserView as TextView).text = nativeAd?.advertiser
+        nativeAd?.mediaContent?.let {
+            adView.mediaView?.setMediaContent(it)
+        }
+        adView.callToActionView = findViewById(R.id.btnAction)
+        adView.setNativeAd(nativeAd)
+        adView.callToActionView = findViewById(R.id.btnAction)
+    }
 }
--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdmobController.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdmobController.kt
@@ -2,12 +2,12 @@ package com.nover.flutternativeadmob
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import com.google.ads.mediation.admob.AdMobAdapter
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.formats.UnifiedNativeAd
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.nativead.NativeAd
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -18,110 +18,111 @@ class NativeAdmobController(
     private val context: Context
 ) : MethodChannel.MethodCallHandler {
 
-  enum class CallMethod {
-    setAdUnitID, reloadAd, setNonPersonalizedAds
-  }
-
-  enum class LoadState {
-    loading, loadError, loadCompleted
-  }
-
-  var nativeAdChanged: ((UnifiedNativeAd?) -> Unit)? = null
-  var nativeAd: UnifiedNativeAd? = null
-    set(value) {
-      field = value
-      invokeLoadCompleted()
+    enum class CallMethod {
+        setAdUnitID, reloadAd, setNonPersonalizedAds
     }
 
-  private var adLoader: AdLoader? = null
-  private var adUnitID: String? = null
-  private var nonPersonalizedAds = false
+    enum class LoadState {
+        loading, loadError, loadCompleted
+    }
 
-  init {
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    when (CallMethod.valueOf(call.method)) {
-      CallMethod.setAdUnitID -> {
-        call.argument<String>("adUnitID")?.let {
-          val isChanged = adUnitID != it
-          adUnitID = it
-
-          if (adLoader == null || isChanged) {
-            val builder = AdLoader.Builder(context, it)
-            adLoader = builder.forUnifiedNativeAd { nativeAd ->
-              this.nativeAd = nativeAd
-            }.withAdListener(object : AdListener() {
-              override fun onAdFailedToLoad(errorCode: Int) {
-                println("onAdFailedToLoad errorCode = $errorCode")
-                channel.invokeMethod(LoadState.loadError.toString(), null)
-              }
-            }).build()
-          }
-          var numberAds: Int? = 1
-          call.argument<Int>("numberAds")?.let { numberAds = it }
-          if (nativeAd == null || isChanged) loadAd(numberAds) else invokeLoadCompleted()
-        } ?: result.success(null)
-      }
-
-
-      CallMethod.reloadAd -> {
-        var numberAds: Int? = 1
-        call.argument<Int>("numberAds")?.let { numberAds = it }
-        call.argument<Boolean>("forceRefresh")?.let {
-          if (it || nativeAd == null) loadAd(numberAds) else invokeLoadCompleted()
+    var nativeAdChanged: ((NativeAd?) -> Unit)? = null
+    var nativeAd: NativeAd? = null
+        set(value) {
+            field = value
+            invokeLoadCompleted()
         }
-      }
 
-      CallMethod.setNonPersonalizedAds -> {
-        call.argument<Boolean>("nonPersonalizedAds")?.let {
-          nonPersonalizedAds = it
+    private var adLoader: AdLoader? = null
+    private var adUnitID: String? = null
+    private var nonPersonalizedAds = false
+
+    init {
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (CallMethod.valueOf(call.method)) {
+            CallMethod.setAdUnitID -> {
+                call.argument<String>("adUnitID")?.let {
+                    val isChanged = adUnitID != it
+                    adUnitID = it
+
+                    if (adLoader == null || isChanged) {
+                        val builder = AdLoader.Builder(context, it)
+                        adLoader = builder.forNativeAd { nativeAd ->
+                            this.nativeAd = nativeAd
+                        }.withAdListener(object : AdListener() {
+                            override fun onAdFailedToLoad(p0: LoadAdError) {
+                                super.onAdFailedToLoad(p0)
+                                println("onAdFailedToLoad errorCode = ${p0.code}")
+                                channel.invokeMethod(LoadState.loadError.toString(), null)
+                            }
+                        }).build()
+                    }
+                    var numberAds: Int? = 1
+                    call.argument<Int>("numberAds")?.let { numberAds = it }
+                    if (nativeAd == null || isChanged) loadAd(numberAds) else invokeLoadCompleted()
+                } ?: result.success(null)
+            }
+
+
+            CallMethod.reloadAd -> {
+                var numberAds: Int? = 1
+                call.argument<Int>("numberAds")?.let { numberAds = it }
+                call.argument<Boolean>("forceRefresh")?.let {
+                    if (it || nativeAd == null) loadAd(numberAds) else invokeLoadCompleted()
+                }
+            }
+
+            CallMethod.setNonPersonalizedAds -> {
+                call.argument<Boolean>("nonPersonalizedAds")?.let {
+                    nonPersonalizedAds = it
+                }
+                result.success(null)
+            }
         }
-        result.success(null)
-      }
     }
-  }
 
-  private fun loadAd(numberAds: Int?) {
-    channel.invokeMethod(LoadState.loading.toString(), null)
-    val requestBuilder: AdRequest.Builder = AdRequest.Builder()
-    if(nonPersonalizedAds){
-      val extras = Bundle().apply {
-        putString("npa", "1")
-      }
-      requestBuilder.addNetworkExtrasBundle(AdMobAdapter::class.java, extras)
+    private fun loadAd(numberAds: Int?) {
+        channel.invokeMethod(LoadState.loading.toString(), null)
+        val requestBuilder: AdRequest.Builder = AdRequest.Builder()
+        if (nonPersonalizedAds) {
+            val extras = Bundle().apply {
+                putString("npa", "1")
+            }
+            requestBuilder.addNetworkExtrasBundle(AdMobAdapter::class.java, extras)
+        }
+        if (numberAds != null && numberAds > 1) {
+            adLoader?.loadAds(requestBuilder.build(), numberAds)
+        } else {
+            adLoader?.loadAd(requestBuilder.build())
+        }
     }
-    if(numberAds != null && numberAds > 1){
-      adLoader?.loadAds(requestBuilder.build(), numberAds)
-    } else {
-      adLoader?.loadAd(requestBuilder.build())
-    }
-  }
 
-  private fun invokeLoadCompleted() {
-    nativeAdChanged?.let { it(nativeAd) }
-    channel.invokeMethod(LoadState.loadCompleted.toString(), null)
-  }
+    private fun invokeLoadCompleted() {
+        nativeAdChanged?.let { it(nativeAd) }
+        channel.invokeMethod(LoadState.loadCompleted.toString(), null)
+    }
 }
 
 object NativeAdmobControllerManager {
-  private val controllers: ArrayList<NativeAdmobController> = arrayListOf()
+    private val controllers: ArrayList<NativeAdmobController> = arrayListOf()
 
-  fun createController(id: String, binaryMessenger: BinaryMessenger, context: Context) {
-    if (getController(id) == null) {
-      val methodChannel = MethodChannel(binaryMessenger, id)
-      val controller = NativeAdmobController(id, methodChannel, context)
-      controllers.add(controller)
+    fun createController(id: String, binaryMessenger: BinaryMessenger, context: Context) {
+        if (getController(id) == null) {
+            val methodChannel = MethodChannel(binaryMessenger, id)
+            val controller = NativeAdmobController(id, methodChannel, context)
+            controllers.add(controller)
+        }
     }
-  }
 
-  fun getController(id: String): NativeAdmobController? {
-    return controllers.firstOrNull { it.id == id }
-  }
+    fun getController(id: String): NativeAdmobController? {
+        return controllers.firstOrNull { it.id == id }
+    }
 
-  fun removeController(id: String) {
-    val index = controllers.indexOfFirst { it.id == id }
-    if (index >= 0) controllers.removeAt(index)
-  }
+    fun removeController(id: String) {
+        val index = controllers.indexOfFirst { it.id == id }
+        if (index >= 0) controllers.removeAt(index)
+    }
 }
